45580a54a95c5a5d11fa73017e9ef6eb577c1f94
Helaman Bennion <helama22@byu.edu>
2024-08-20 16:03:36 -0600

    include css

--- src/index.js
+++ src/index.js
@@ -16,20 +16,19 @@ const allowCrossDomain = (req, res, next) => {
     next();
 };
 
-// Serve up the static content
-app.use(express.static('public'));
-
 app.use(express.json());
 
 app.use(allowCrossDomain)
 
 const logger = (req, res, next) => {
     console.log(`RECEIVED ${req.method} REQUEST`);
-    console.log(`DateTime: ${new Date()}`)
+    console.log(`Host: ${req.rawHeaders[1]}; DateTime: ${new Date()}`)
     next();
 }
 
 app.use(logger);
+
+app.use(express.static('public'))
 
 app.use((_req, res) => {
     res.sendFile('./public/index.html', { root: './' });

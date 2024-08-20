4af7b827b7db4ac114a8041acda2f8e2eac4af38
Helaman Bennion <helama22@byu.edu>
2024-08-19 18:00:14 -0600

    setup to run with express instead of fastify

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
+const express = require('express');
 
-// CommonJs
-const fastify = require('fastify')({
-    logger: true
-})
+/*** SERVER ROUTING ***/
+const app = express();
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-})
+// The service port defaults to 3000 or is read from the program arguments
+const port = process.argv.length > 2 ? process.argv[2] : 5000;
 
-// Run the server!
-fastify.listen({ port: 5000, host: '0.0.0.0' }, function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-    // Server is now listening on ${address}
-})
+// Text to display for the service name
+const serviceName = process.argv.length > 3 ? process.argv[3] : 'website';
+
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `0.0.0.0`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+};
+
+// Serve up the static content
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(allowCrossDomain)
+
+const logger = (req, res, next) => {
+    console.log(`RECEIVED ${req.method} REQUEST`);
+    console.log(`DateTime: ${new Date()}`)
+    next();
+}
+
+app.use(logger);
+
+app.use((_req, res) => {
+    res.sendFile('./public/index.html', { root: './' });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
